1107eb42cd5e556b73275cd1892a36ad5677258c
ZinMarLwin <zimmar07@gmail.com>
2026-09-17 13:48:01 +0900

    Add React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import UserListPage from './pages/UserListPage'
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <UserListPage />
+    </>
+  )
+}
+
+export default App
